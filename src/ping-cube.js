237c7b70cbb61309b3831590e137bbb3715f2f43
AmurKhoyetsyan <javanewss@gmail.com>
2023-10-15 15:48:45 +0400

    add ping-cube loader

---
+++ src/ping-cube.js
@@ -0,0 +1,226 @@
+/**
+ * Copyright (c) Amur 2020
+ *
+ * Smart Loaders
+ * https://github.com/AmurKhoyetsyan/smart-loaders
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+;(function () {
+    /**
+     * @param item
+     */
+    let createTitle = item => {
+        if (item.hasAttribute('title')) {
+            let color = item.hasAttribute('title-color') ? item.getAttribute('title-color') : "#FFFFFF";
+            let loaderTitle = document.createElement('DIV');
+            loaderTitle.classList.add('sl-loader-title');
+            loaderTitle.setAttribute("style", `--cl: ${color}`);
+            let span = document.createElement('SPAN');
+            span.innerText = item.getAttribute('title');
+            loaderTitle.appendChild(span);
+            item.appendChild(loaderTitle);
+        }
+    };
+
+    /**
+     * @param count
+     * @returns {string|number}
+     */
+    const getZoom = count => {
+        let size = parseFloat(count);
+        if (size === 0) {
+            return 0;
+        }
+
+        let zoom = size / 100;
+        return zoom.toFixed(2);
+    };
+
+    /**
+     * @param item
+     * @returns {string}
+     */
+    const getBackground = item => item.hasAttribute('loader-color') ? item.getAttribute('loader-color') : "#FFFFFF";
+
+    /**
+     * @param item
+     * @param index
+     * @param classes
+     * @param itemClasses
+     * @param start
+     * @param end
+     * @param typeLoader
+     */
+    const pingCubeLoader = (item, index, classes, itemClasses, start, end, typeLoader) => {
+        let loaderItems = document.createElement('DIV');
+        loaderItems.classList.add('sl-loader-items');
+
+        let loader = document.createElement('DIV');
+        loader.classList.add('sl-loader');
+
+        let size = item.hasAttribute('size') ? getZoom(item.getAttribute('size')) : 1;
+        loader.setAttribute("style", `--size: ${size}`);
+
+        for (let i = start; i <= end; i++) {
+            let pingCube = document.createElement('DIV');
+            pingCube.classList.add(itemClasses);
+            pingCube.setAttribute("style", `--i: ${i}; --bg: ${getBackground(item)}`);
+            loaderItems.appendChild(pingCube);
+        }
+
+        loader.appendChild(loaderItems);
+
+        let style = document.createElement('STYLE');
+        style.setAttribute('type', 'text/css');
+        style.innerText = "[data-loader],[data-loader] *{padding:0;margin:0;-webkit-box-sizing:border-box;box-sizing:border-box}[data-loader] .sl-loader-title{width:100%;text-align:center;font-size:17px;font-weight:700;line-height:1.7;color:var(--cl)}.sl-ping-cube,[data-loader]{display:-webkit-box;display:-ms-flexbox;display:flex;-webkit-box-orient:vertical;-webkit-box-direction:normal;-ms-flex-direction:column;flex-direction:column;-ms-flex-wrap:nowrap;flex-wrap:nowrap;-webkit-box-align:center;-ms-flex-align:center;align-items:center;-webkit-box-pack:center;-ms-flex-pack:center;justify-content:center}.sl-ping-cube .sl-loader{width:100px}.sl-ping-cube .sl-loader .sl-loader-items{width:100%;height:40px;position:relative;display:-webkit-box;display:-ms-flexbox;display:flex;-webkit-box-orient:horizontal;-webkit-box-direction:normal;-ms-flex-direction:row;flex-direction:row;-ms-flex-wrap:nowrap;flex-wrap:nowrap;-webkit-box-pack:center;-ms-flex-pack:center;justify-content:center;-webkit-box-align:center;-ms-flex-align:center;align-items:center}.sl-ping-cube .sl-loader .sl-loader-items .sl-ping-cube{width:14px;height:25px;background-color:var(--bg);-webkit-animation:1s ease-in-out infinite loader-ping-cube;animation:1s ease-in-out infinite loader-ping-cube;-webkit-animation-delay:calc(var(--i) * 150ms);animation-delay:calc(var(--i) * 150ms)}.sl-ping-cube .sl-loader .sl-loader-items .sl-ping-cube+.sl-ping-cube{margin-left:7px}@-webkit-keyframes loader-ping-cube{50%,from,to{-webkit-transform:scale(1,1);transform:scale(1,1)}30%,5%{-webkit-transform:scale(1,1.5);transform:scale(1,1.5)}}@keyframes loader-ping-cube{50%,from,to{-webkit-transform:scale(1,1);transform:scale(1,1)}30%,5%{-webkit-transform:scale(1,1.5);transform:scale(1,1.5)}}";
+
+
+        item.appendChild(style);
+
+        let pingCube = document.createElement('DIV');
+        pingCube.classList.add(classes);
+        pingCube.appendChild(loader);
+        item.appendChild(pingCube);
+        createTitle(item);
+    };
+
+    let mutator = {
+        option: {
+            childList: true,
+            attributes: true,
+            characterData: true,
+            subtree: true
+        },
+        observer: null,
+        loaders: null,
+        olderLoaders: []
+    };
+
+    /**
+     * @param attr
+     * @returns {string}
+     */
+    mutator.attrListString = function (attr) {
+        let str = "";
+        if (attr.length === 0) {
+            return str;
+        }
+
+        for (let item of attr) {
+            str += `${item.name}: ${item.value}; `;
+        }
+
+        return str.trim();
+    };
+
+    /**
+     * @param node1
+     * @param node2
+     * @returns {boolean}
+     */
+    mutator.equalsNode = function (node1, node2) {
+        let attr1 = node1.attributes;
+        let attr2 = node2.attributes;
+
+        let len1 = attr1.length;
+
+        if (len1 !== attr2.length) {
+            return false;
+        }
+
+        if (mutator.attrListString(attr1) !== mutator.attrListString(attr2)) {
+            return false;
+        }
+
+        return true;
+    };
+
+    mutator.equals = function () {
+        this.loaders = document.querySelectorAll('[data-loader="ping-cube"]');
+
+        if (this.loaders === null) {
+            return false;
+        }
+
+        if (this.olderLoaders.length !== this.loaders.length) {
+            return false;
+        }
+
+        for (let [index, item] of this.loaders.entries()) {
+            if (!this.equalsNode(item, this.olderLoaders[index])) {
+                return false;
+            }
+        }
+
+        return true;
+    };
+
+    /**
+     * @param item
+     */
+    mutator.removeAnotherLoaders = function (item) {
+        while (item.firstChild) {
+            item.removeChild(item.firstChild);
+        }
+    };
+
+    mutator.disconnect = function () {
+        if (!this.observer) {
+            return false;
+        }
+
+        this.observer.disconnect();
+    };
+
+    mutator.replaceLoader = function () {
+        if (mutator.loaders && !mutator.equals()) {
+            mutator.disconnect();
+            mutator.loaders.forEach((item, index) => mutator.removeAnotherLoaders(item));
+            mutator.addLoaders();
+            mutator.start();
+        }
+    };
+
+    /**
+     * @param nodeList
+     */
+    mutator.cloneNodeList = function (nodeList) {
+        this.olderLoaders = [];
+        nodeList.forEach((item, index) => {
+            this.olderLoaders.push(item.cloneNode(true));
+        });
+    };
+
+    mutator.addLoaders = function () {
+        let loaders = document.querySelectorAll('[data-loader="ping-cube"]');
+        
+        if (loaders !== null) {
+            this.loaders = loaders;
+            this.cloneNodeList(loaders);
+        }
+    };
+
+    mutator.connect = function () {
+        this.observer = new MutationObserver(this.replaceLoader);
+    };
+
+    mutator.start = function () {
+        if (!this.observer) {
+            this.connect();
+            this.addLoaders();
+        }
+
+        if (this.loaders.length > 0) {
+            this.loaders.forEach((item, index) => pingCubeLoader(item, index, 'sl-ping-cube', 'sl-ping-cube', 1, 3, 'pingCube'));
+        }
+
+        if (this.observer) {
+            this.observer.observe(document, this.option);
+        }
+    };
+
+    mutator.start();
+})();
